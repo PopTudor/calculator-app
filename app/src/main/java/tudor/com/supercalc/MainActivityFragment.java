3d63d918f4b05f6044ce2903b0f284e690193840
Tudor <tudor08pop@yahoo.com>
2015-05-18 19:40:45 +0300

    finished portrait/landscape layout

--- app/src/main/java/tudor/com/supercalc/MainActivityFragment.java
+++ app/src/main/java/tudor/com/supercalc/MainActivityFragment.java
@@ -12,12 +12,17 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+
+
 	public MainActivityFragment() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_main, container, false);
+		View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+		return view;
 	}
 }
